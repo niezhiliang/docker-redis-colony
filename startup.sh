1f557ae995a6f3062ec3b737faee323b8d56c812
聂志良 <1132263176@qq.com>
2019-02-22 17:26:29 +0800

    m

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#集群文件防止目录
+redisdir="/home/redis-cluster"
+
 for port in `seq 7000 7005`; do \
   docker run -d -ti -p ${port}:${port} -p 1${port}:1${port} \
   -v ${redisdir}/${port}/conf/redis.conf:/usr/local/etc/redis/redis.conf \
